f95165ebb0374c3955ccbed439c59f6d134684ab
Mario <mavega@uvm.edu>
2020-10-04 15:49:34 -0400

    finished del 31,32,33,34,35

--- PredictGestures.js
+++ PredictGestures.js
@@ -1,9 +1,16 @@
 var trainingCompleted = false;
 
 function Train(){
-  console.log(train0);
-  console.log(test);
- 
+//  console.log(train0);
+//  console.log(test);
+  for (var i = 0; i < train0.shape[3]; i++) {
+      //console.log(train0.pick(null, null, null, i).toString());
+      var features = train0.pick(null,null,null,null,i);
+      //reshape 
+      features = features.reshape(120);
+      //console.log(features.toString());
+      
+  }
 }
 function Test(){
     
