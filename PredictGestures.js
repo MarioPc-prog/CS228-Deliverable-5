25379f0271fa969db26f28e3605ce2dc864d8244
Mario <mavega@uvm.edu>
2020-10-05 22:13:46 -0400

    Fixed bug done

--- PredictGestures.js
+++ PredictGestures.js
@@ -5,45 +5,32 @@ var predictedClassLabels = nj.zeros(2);
 const knnClassifier = ml5.KNNClassifier();
 
 function Train(){
-    //console.log(train0);
-    //console.log(test);
     trainingCompleted = true;
-    for (var i = 0; i < train7.shape[3]; i++) {
-        var features = train7.pick(null, null, null, i).reshape(120);
-        //console.log(features);
+    for (var i = 0; i < train7.shape[3]; i++) {   
+        var features = train7.pick(null, null, null, i).reshape(1,120);
         knnClassifier.addExample(features.tolist(),7);
-        var features2 = train9.pick(null, null, null, i).reshape(120);
-        //console.log(features2);
-        knnClassifier.addExample(features2.tolist(),9);
-//      //console.log(features);
-//      features = train1.pick(null,null,null,i).reshape(1,120);
-//      knnClassifier.addExample(features.tolist(),1);
-//      //console.log(features);
+        features = train9.pick(null, null, null, i).reshape(1,120);
+        knnClassifier.addExample(features.tolist(),9);
   }
 }
 
 function Test(){
       var currentFeatures =  test.pick(null,null,null,testingSampleIndex).reshape(1,120);
       knnClassifier.classify(currentFeatures.tolist(),GotResults);
-      //var currentFeatures2 =  train7.pick(null,null,null,testingSampleIndex).reshape(1,120);
-      //knnClassifier.classify(currentFeatures2.tolist(),GotResults);
       
 }
 function GotResults(err, result){
-    //console.log(testingSampleIndex);
       predictedClassLabels.set(testingSampleIndex, parseInt(result.label));
       console.log(testingSampleIndex + ": " + predictedClassLabels.get(testingSampleIndex));
       testingSampleIndex += 1;
-      if (testingSampleIndex >=2){
+      if (testingSampleIndex >=100){
           testingSampleIndex = 0;
       }
 }
 function draw(){
     clear();
      if (trainingCompleted === false){
-         Train();
-         trainingCompleted = true ; 
-         
+         Train();    
     }
-    //Test();
+    Test();
  } 
